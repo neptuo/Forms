c5ad4822c938b94b2474661e95053ad8c1936ebb
Marek Fišera <maraf@users.noreply.github.com>
2012-04-07 10:26:11 +0000

    Initial project structure

---
+++ Neptuo.Forms.Web/BootstrapperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Neptuo.Web.Localization;
+using Neptuo.Web.Localization.Mvc3;
+using Neptuo.Web.Mvc;
+
+namespace Neptuo.Forms.Web
+{
+    public static class BootstrapperExtensions
+    {
+        public static Bootstrapper BuildLocalization(this Bootstrapper bootstrapper, Action<IResourceService> initializeService)
+        {
+            LocalizationService.SetProviders();
+            if (initializeService != null)
+                initializeService(LocalizationService.Current);
+
+            return bootstrapper;
+        }
+    }
+}
---
+++ Neptuo.Forms.Web/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Practices.Unity;
+using Neptuo.Web.Localization;
+using Neptuo.Web.Mvc;
+
+namespace Neptuo.Forms.Web
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+
+        private static void RegisterUnity(UnityContainer container)
+        {
+
+        }
+
+        private static void RegisterLocales(IResourceService service)
+        {
+
+        }
+
+        protected void Application_Start()
+        {
+            Bootstrapper
+                .Initialize()
+                .BuildUnity(RegisterUnity)
+                .BuildLocalization(RegisterLocales)
+                .SetupAreas()
+                .SetupGlobalFilters()
+                .SetupRoutes(RegisterRoutes)
+                .SetupViewEngine(true);
+        }
+    }
+}
