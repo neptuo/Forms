624bc57f981cd30e4b1043eedcda8c4dffc2a803
Marek Fišera <maraf@users.noreply.github.com>
2012-04-30 18:07:41 +0000

    Cloud settings

---
+++ Neptuo.Forms.Core/Service/AzureBlobLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Neptuo.Forms.Core.Service
+{
+    public class AzureBlobLogger : ILogger
+    {
+        public void Log(string message, params object[] parameters)
+        {
+            message = String.Format(message, parameters);
+        }
+    }
+}
